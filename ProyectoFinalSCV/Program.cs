aa388f117d10e09c3e42613608c281050f1fadbe
ejmorales <eduardojmorales2@gmail.com>
2020-12-22 16:57:57 -0600

    Borrado de codigo en conflicto.

--- ProyectoFinalSCV/Program.cs
+++ ProyectoFinalSCV/Program.cs
@@ -28,17 +28,7 @@ namespace ProyectoFinalSCV
                     b = double.Parse(Console.ReadLine());
                     Console.WriteLine();
                     Console.WriteLine($"El resultado de la suma es: {a+b}");
-                    break;
-                case 4:
-                    double g, h;
-                    Console.WriteLine("--- DIVISION ---");
-                    Console.WriteLine("Ingrese el primer número:");
-                    g = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Ingrese el segundo número:");
-                    h = double.Parse(Console.ReadLine());
-                    Console.WriteLine();
-                    Console.WriteLine($"El resultado de la división es: {g/h}");
-                    break;
+                    break;                
                 default:
                     break;
             }
