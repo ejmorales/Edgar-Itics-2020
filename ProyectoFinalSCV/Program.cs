9ee3e23a54667973552c03af005defd784434705
ejmorales <eduardojmorales2@gmail.com>
2020-12-22 11:24:39 -0600

    Agregado de variable y opcion a elegir.

--- ProyectoFinalSCV/Program.cs
+++ ProyectoFinalSCV/Program.cs
@@ -6,13 +6,16 @@ namespace ProyectoFinalSCV
     {
         static void Main(string[] args)
         {
+            int opc = 0;
             Console.WriteLine("----- MENU DE OPCIONES -----");
             Console.WriteLine("1 - SUMA");
             Console.WriteLine("2 - RESTA");
             Console.WriteLine("3 - MULTIPLICACION");
             Console.WriteLine("4 - DIVISION");
+            Console.WriteLine(" 5 - SALIR");
             Console.WriteLine();
             Console.WriteLine("Elija una opción: ");
+            opc = int.Parse(Console.ReadLine());
         }
     }
 }
