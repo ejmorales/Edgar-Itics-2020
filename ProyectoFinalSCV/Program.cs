8007f656ca2f6491f629497246094587c5251cb2
ejmorales <eduardojmorales2@gmail.com>
2020-12-22 11:16:15 -0600

    Inicio al seguimiento del proyecto.

---
+++ ProyectoFinalSCV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoFinalSCV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
